af3b278cd95b04190f6d95d37defd87808b3063d
Sune Marcher <snemarch@gmail.com>
2020-06-20 17:39:45 +0200

    Add SMFilter for Response element handling
    Performance seems around the same, sometimes slightly better, than the
    previous solution. Slightly bigger fluctuations for Aalto than Woodstox.

--- src/main/java/dk/snemarch/xmltest/ApplicationResponseExtractorSM.kt
+++ src/main/java/dk/snemarch/xmltest/ApplicationResponseExtractorSM.kt
@@ -1,15 +1,30 @@
 package dk.snemarch.xmltest
 
 import org.codehaus.staxmate.SMInputFactory
+import org.codehaus.staxmate.`in`.SMEvent
+import org.codehaus.staxmate.`in`.SMFilter
 import org.codehaus.staxmate.`in`.SMInputCursor
 import java.io.InputStream
 import java.time.LocalDate
 import java.util.*
 import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.XMLStreamConstants.END_ELEMENT
+import javax.xml.stream.XMLStreamConstants.START_ELEMENT
 
+/*
+Structure we're interested in:
+
+ApplicationResponse			[1..1]
+	IssueDate				[1..1]
+	DocumentResponse		[0..*]
+		Response			[1..1]
+			ResponseCode	[0..1]
+			Description		[0..*]
+ */
 class ApplicationResponseExtractorSM {
 	companion object {
 		private val FACTORY = SMInputFactory(initializeFactory())
+		private val responseFilter = NamedElementFilter("Response")
 
 		private fun initializeFactory(): XMLInputFactory {
 			val factory = XMLInputFactory.newInstance()
@@ -32,7 +47,10 @@ class ApplicationResponseExtractorSM {
 			while(child.next != null) {
 				when(child.localName) {
 					"IssueDate" -> issueDate = LocalDate.parse(child.elemStringValue)
-					"DocumentResponse" -> handleDocumentResponses(child.childElementCursor())
+
+					"DocumentResponse" ->
+						// Response is [1..1], so we don't have to iterate but can just advance() into children.
+						handleResponses(child.childCursor(responseFilter).advance().childElementCursor())
 				}
 			}
 		} finally {
@@ -46,21 +64,20 @@ class ApplicationResponseExtractorSM {
 		return ExtraDataApplicationResponse(UUID.randomUUID(), issueDate!!, responseCode!!, description!!)
 	}
 
-	private fun handleDocumentResponses(documentResponse: SMInputCursor) {
+	private fun handleResponses(response: SMInputCursor) {
 		++numDocumentResponses
-		while(documentResponse.next != null) {
-			when(documentResponse.localName) {
-				"Response" -> handleResponse(documentResponse.childElementCursor())
-			}
-		}
-	}
-
-	private fun handleResponse(response: SMInputCursor) {
 		while(response.next != null) {
-			when(response.localName) {
+			when (response.localName) {
 				"ResponseCode" -> responseCode = response.elemStringValue
 				"Description" -> description = response.elemStringValue
 			}
 		}
+	}
+}
+
+class NamedElementFilter(private val name: String):	SMFilter() {
+	override fun accept(evt: SMEvent, caller: SMInputCursor) = when(evt.eventCode) {
+		START_ELEMENT, END_ELEMENT -> caller.localName == name
+		else -> false
 	}
 }
